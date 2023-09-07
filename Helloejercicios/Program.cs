fbea18e0d0f9cdca4f412facd6b014bf6598230a
Tatiana <tarango.miro@serviciosnutresa.com>
2023-09-07 12:19:34 -0500

    Remove ejercicio 1

--- Helloejercicios/Program.cs
+++ Helloejercicios/Program.cs
@@ -1,67 +1,26 @@
 ﻿// Ejercicio 1 Positive Power 
 // Solicita al usuario un número y eleva este número al cuadrado.
-
-using System;
-
-namespace ConsoleApp
+Console.Write("Ingresa un número: ");
+if (double.TryParse(Console.ReadLine(), out double numero))
 {
-    class Program
+    double resultado;
+
+    if (numero > 0)
     {
-        static void Main(string[] args)
-        {
-            Ejercicio1();
-            Ejercicio2();
-            Ejercicio3();
-        }
-
-        static void Ejercicio1()
-        {
-            Console.Write("Ingresa un número: ");
-            if (double.TryParse(Console.ReadLine(), out double numero))
-            {
-                string resultado = (numero > 0) ? Math.Pow(numero, 2).ToString() : (numero < 0) ? "Número negativo." : "0";
-                Console.WriteLine($"Resultado: {resultado}");
-            }
-            else
-            {
-                Console.WriteLine("Entrada no válida. Ingresa un número válido.");
-            }
-        }
-
-        static void Ejercicio2()
-        {
-            Console.Write("Ingresa el primer número: ");
-            if (double.TryParse(Console.ReadLine(), out double numero1))
-            {
-                Console.Write("Ingresa el segundo número: ");
-                if (double.TryParse(Console.ReadLine(), out double numero2))
-                {
-                    double resultado = (numero1 > numero2) ? numero1 * 2 : numero2 * 3;
-                    Console.WriteLine($"Resultado: {resultado}");
-                }
-                else
-                {
-                    Console.WriteLine("Entrada no válida para el segundo número.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Entrada no válida para el primer número.");
-            }
-        }
-
-        static void Ejercicio3()
-        {
-            Console.Write("Ingresa un número: ");
-            if (double.TryParse(Console.ReadLine(), out double numero3))
-            {
-                double resultado = (numero3 > 0) ? Math.Sqrt(numero3) : Math.Pow(numero3, 2);
-                Console.WriteLine($"Resultado: {resultado}");
-            }
-            else
-            {
-                Console.WriteLine("Entrada no válida. Ingresa un número válido.");
-            }
-        }
+        resultado = Math.Pow(numero, 2);
     }
+    else if (numero < 0)
+    {
+        resultado = numero;
+    }
+    else
+    {
+        resultado = 0;
+    }
+
+    Console.WriteLine($"Resultado: {resultado}");
+}
+else
+{
+    Console.WriteLine("Entrada no válida. Ingresa un número válido.");
 }
