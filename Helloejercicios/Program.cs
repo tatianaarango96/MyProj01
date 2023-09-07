3beb81f45bf9fa3c1a43e519b7a47a78ef35a5c2
Tatiana <tarango.miro@serviciosnutresa.com>
2023-09-07 12:46:36 -0500

    ejercicio2

--- Helloejercicios/Program.cs
+++ Helloejercicios/Program.cs
@@ -24,3 +24,24 @@ else
 {
     Console.WriteLine("Entrada no válida. Ingresa un número válido.");
 }
+// Ejercicio 2  Double or Tripl
+// Solicita al usuario dos números. Si el primero es mayor, devuelva 
+//su doble, de lo contrario devuelva el triple del segundo. 
+ Console.Write("Ingresa el primer número: ");
+if (double.TryParse(Console.ReadLine(), out double numero1))
+{
+    Console.Write("Ingresa el segundo número: ");
+    if (double.TryParse(Console.ReadLine(), out double numero2))
+    {
+        double resultado = (numero1 > numero2) ? numero1 * 2 : numero2 * 3;
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida para el segundo número.");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no válida para el primer número.");
+}
